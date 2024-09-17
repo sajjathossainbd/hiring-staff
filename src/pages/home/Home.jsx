310ad97e44b476174876e5fb2549bc37cebc2429
Israfil6580 <israfilhossainmilon@gmail.com>
2024-09-18 00:56:19 +0600

    add: create banner component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,10 @@
+import Banner from "./Banner";
+
 function Home() {
   return (
-    <div>
-      <h2>
-        The <span className="text-blue">Easiest Way </span>
-        <br />
-        to Get Your New Job
-      </h2>
-      <p className="text-sm">Location</p>
-      <p className="text-lg">
-        Each month, more than 3 million job seekers turn to website in their{" "}
-        <br />
-        search for work, making over 140,000 applications every single day
-      </p>
-      <p className="text-lightGray-12">0 Vacancies</p>
-      <h3>Browse by category</h3>
-      <strong>Content Writer</strong>
-    </div>
+    <>
+      <Banner />
+    </>
   );
 }
 

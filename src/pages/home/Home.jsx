8a836b221667f58f27290a51de68a31fccd79cb1
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-17 18:46:08 +0600

    design system create

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,21 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>
+        The <span className="text-[#3C65F5]">Easiest Way </span>
+        <br />
+        to Get Your New Job
+      </h2>
+      <p className="text-lg">
+        Each month, more than 3 million job seekers turn to website in their{" "}
+        <br />
+        search for work, making over 140,000 applications every single day
+      </p>
+      <p className="text-gray">0 Vacancies</p>
+      <h3>Browse by category</h3>
+      <strong>Content Writer</strong>
+    </div>
+  );
 }
 
 export default Home;

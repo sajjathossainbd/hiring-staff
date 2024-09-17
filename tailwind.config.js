e1f77fa67b7dad800b860ebe85e424d463dfb082
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-17 22:02:22 +0600

    update: create a desing system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blue: "#3C65F5",
+      darkBlue: "#05274F",
+      gray: "#4F5E64",
+      lightGray: "#A0Abb8",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],

9a95cf0ae6a6bbab5730f7f35c7153628ccb7f70
dev-kawser <kawserofficial.dev@gmail.com>
2024-09-19 03:21:16 +0600

    Add: pricing card component create and use in pricing section

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -2,12 +2,13 @@ import { IoHomeOutline } from 'react-icons/io5';
 import './pricing.css'
 import { IoIosArrowForward } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+import PricingCard from '../../components/shared/PricingCard';
 
 const Pricing = () => {
     return (
         <div>
-            <div className="banner py-14">
-                <div className='container mx-auto flex items-center justify-between'>
+            <div className="banner">
+                <div className='container mx-auto flex lg:flex-row flex-col gap-3 items-center justify-between'>
                     <div>
                         <h3>
                             Pricing
@@ -25,7 +26,43 @@ const Pricing = () => {
                         </p>
                     </div>
                 </div>
-                <div className='max-w-4xl'>
+            </div>
+            <div className='max-w-6xl mx-auto lg:mt-20 mt-10 px-2'>
+                <div className='text-center space-y-2'>
+                    <h3>Pricing Table</h3>
+                    <p className='text-18'>Choose The Best Plan That’s For You</p>
+                </div>
+                <div className='mt-10 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 lg:gap-10'>
+                    <PricingCard
+                        category={"Basic"}
+                        price={"19"}
+                        description={"For most businesses that want to optimize web queries"}
+                        service1={"Unlimited updates"}
+                        service2={"Custom designs & features"}
+                        service3={"Custom instructors"}
+                        service4={"Custom permissions"}
+                        service5={"Free support ticket"}
+                    />
+                    <PricingCard
+                        category={"Basic"}
+                        price={"29"}
+                        description={"For most businesses that want to optimize web queries"}
+                        service1={"Unlimited updates"}
+                        service2={"Custom designs & features"}
+                        service3={"Custom instructors"}
+                        service4={"Custom permissions"}
+                        service5={"Free support ticket"}
+                    />
+                    <PricingCard
+                        category={"Basic"}
+                        price={"39"}
+                        description={"For most businesses that want to optimize web queries"}
+                        service1={"Unlimited updates"}
+                        service2={"Custom designs & features"}
+                        service3={"Custom instructors"}
+                        service4={"Custom permissions"}
+                        service5={"Free support ticket"}
+                    />
 
                 </div>
             </div>

499fc528e9445f7fede8cc2ecdf1fd5b62c1040b
dev-kawser <kawserofficial.dev@gmail.com>
2024-09-18 11:01:14 +0600

    Add: added a footer

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,5 +1,79 @@
-function Footer() {
-  return <div>Footer</div>;
-}
+import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+import logo from "../../assets/logo/hiring.png"
+import appStore from "../../assets/footer-images/app-store.svg"
+import googlePlay from "../../assets/footer-images/google-play.svg"
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer text-base-content lg:p-0 p-10">
+        <aside className="space-y-3">
+          <div className="flex items-center gap-2">
+            <img className="size-12" src={logo} alt="" />
+            <h3>Hiring Stuff</h3>
+          </div>
+          <p className="max-w-72">
+            Hiring Stuff is the heart of the design community and the best resource to discover and connect with designers and jobs worldwide
+          </p>
+          <div className="flex items-center gap-3 text-2xl">
+            <FaFacebook />
+            <FaTwitter />
+            <FaLinkedin />
+          </div>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Resources</h6>
+          <a className="link link-hover">About Us</a>
+          <a className="link link-hover">Candidates</a>
+          <a className="link link-hover">Contact Us</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Community</h6>
+          <a className="link link-hover">Feature</a>
+          <a className="link link-hover">Pricing</a>
+          <a className="link link-hover">Credit</a>
+          <a className="link link-hover">FAQ</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Quick links</h6>
+          <a className="link link-hover">IOS</a>
+          <a className="link link-hover">Android</a>
+          <a className="link link-hover">Desktop</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">More</h6>
+          <a className="link link-hover">Helps</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Terms</a>
+        </nav>
+        <div className="space-y-3">
+          <h6 className="footer-title">
+            Download App
+          </h6>
+          <p className="max-w-52">
+            Download our Apps and get extra 15% Discount on your first Order…!
+          </p>
+          <div className="flex items-center gap-1">
+            <img src={appStore} alt="" />
+            <img src={googlePlay} alt="" />
+          </div>
+        </div>
+      </footer>
+
+      <div className="divider"></div>
+
+      <div className="flex lg:flex-row flex-col items-center justify-between gap-2">
+        <strong>Copyright © {new Date().getFullYear()} - Hiring Stuff all right reserved</strong>
+        <div className="flex items-center gap-3 lg:gap-10">
+          <strong>Privacy Policy</strong>
+          <strong>Terms & Conditions</strong>
+          <strong>Security</strong>
+        </div>
+      </div>
+
+    </div>
+  );
+};
 
 export default Footer;

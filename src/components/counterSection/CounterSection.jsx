8196573e195cbce31842601c35f3f96d341b5fb2
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-09-19 18:25:30 +0600

    testing purpose

--- src/components/counterSection/CounterSection.jsx
+++ src/components/counterSection/CounterSection.jsx
@@ -1,45 +1,42 @@
 import  { useState, useEffect } from "react";
 
 const CounterSection = () => {
-  // Data array for title and target values
-  const data = [
-    { title: "Completed Cases", targetCount: 25000, duration: 2000 }, // 5 seconds
-    { title: "Our Offices", targetCount: 17, duration: 2000 }, // 3 seconds
-    { title: "Skilled People", targetCount: 86, duration: 2000 }, // 4 seconds
-    { title: "Happy Clients", targetCount: 28, duration: 2000 }, // 2 seconds
-  ];
 
-  // Array to store all the counts
+  const data = [
+    { title: "Completed Cases", targetCount: 25000, duration: 2000 }, 
+    { title: "Our Offices", targetCount: 17, duration: 2000 }, 
+    { title: "Skilled People", targetCount: 86, duration: 2000 }, 
+    { title: "Happy Clients", targetCount: 28, duration: 2000 }, 
+  ]
   const [counts, setCounts] = useState(data.map(() => 0));
 
   useEffect(() => {
     const countUp = (target, index, duration) => {
-      const startTime = performance.now(); // Record the start time
+      const startTime = performance.now(); 
 
       const step = (currentTime) => {
-        const elapsed = currentTime - startTime; // Calculate elapsed time
-        const progress = Math.min(elapsed / duration, 1); // Calculate progress ratio
-        const newCount = Math.floor(progress * target); // Calculate the new count
+        const elapsed = currentTime - startTime; 
+        const progress = Math.min(elapsed / duration, 1); 
+        const newCount = Math.floor(progress * target); 
 
         setCounts((prevCounts) =>
           prevCounts.map((count, idx) => (idx === index ? newCount : count))
         );
 
         if (progress < 1) {
-          requestAnimationFrame(step); // Continue updating until target is reached
+          requestAnimationFrame(step); 
         }
       };
 
-      requestAnimationFrame(step); // Start the animation
+      requestAnimationFrame(step); 
     };
 
-    // Start all counters
     data.forEach((item, index) => {
       countUp(item.targetCount, index, item.duration);
     });
   }, []);
 
-  // Function to format the count with 'K' if the count is 1000 or more
+  
   const formatCount = (count) => {
     if (count >= 1000) {
       return `${Math.floor(count / 1000)}K`;

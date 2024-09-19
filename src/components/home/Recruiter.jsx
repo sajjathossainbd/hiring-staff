3f34d83a487999d42c317bdeed5c66eceddbf9d9
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-19 13:31:23 +0600

    improve: reacruiter component swiper js added

--- src/components/home/Recruiter.jsx
+++ src/components/home/Recruiter.jsx
@@ -1,6 +1,7 @@
 import RecruiterCard from "../shared/RecruiterCard";
 import SectionTitle from "../shared/SectionTitle";
-
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 function Recruiter() {
   const topRecruiters = [
     {
@@ -285,6 +286,16 @@ function Recruiter() {
     },
   ];
 
+  const chunkArray = (arr, size) => {
+    const chunked = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunked.push(arr.slice(i, i + size));
+    }
+    return chunked;
+  };
+
+  const recruitersInSlides = chunkArray(topRecruiters, 12);
+
   return (
     <div className="container">
       <SectionTitle
@@ -293,11 +304,61 @@ function Recruiter() {
           "Discover your next career move, freelance gig, or internship"
         }
       />
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1 gap-3 my-10">
-        {topRecruiters.map((recruiter) => (
-          <RecruiterCard key={recruiter.name} {...recruiter} />
-        ))}
+      <div className="absolute top-0 right-0 flex gap-2 z-10 mt-[-40px]">
+        <button className="swiper-button-prev-custom bg-gray-200 p-3 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </button>
+        <button className="swiper-button-next-custom bg-gray-200 p-3 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5L15.75 12l-7.5 7.5"
+            />
+          </svg>
+        </button>
       </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {recruitersInSlides.map((recruitersGroup, index) => (
+          <SwiperSlide key={index}>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1 gap-3 my-10">
+              {recruitersGroup.map((recruiter) => (
+                <RecruiterCard key={recruiter.name} {...recruiter} />
+              ))}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }

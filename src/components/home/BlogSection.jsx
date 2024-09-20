a2eeadd7fbcf2a18e657039373f23d8321e8f23a
Israfil6580 <israfilhossainmilon@gmail.com>
2024-09-20 12:07:24 +0600

    Update : update blog section

--- src/components/home/BlogSection.jsx
+++ src/components/home/BlogSection.jsx
@@ -14,34 +14,42 @@ const BlogSection = () => {
 
   return (
     <section className="container">
-      <div className=" ">
+      <div>
         <SectionTitle
           title={"News and Blog"}
           subTitle={"Get the latest news, updates and tips"}
         />
 
         <div className="flex flex-wrap gap-4 justify-center mt-14">
-          {/* Job List */}
-          <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-            {blogs.map((blog, id) => (
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            {blogs.map((blog) => (
               <div
-                key={id}
-                className="card bg-base-100 lg:w-96 shadow-xl hover:-translate-y-1 transition duration-300"
+                key={blog.id}
+                className="card bg-base-100 border border-[#D2D4D7] shadow-xl hover:-translate-y-1 transition duration-300 pb-5"
               >
-                <figure className="p-3">
-                  <img className="rounded-lg" src={blog.image} alt="Shoes" />
+                <figure className="p-4">
+                  <img className="rounded-lg" src={blog.image} alt="Blog" />
                 </figure>
-                <p className="ml-3 text-[14px] text-[#3C65F5] badge badge-outline rounded bg-[#E0E6F7]">
-                  {blog.topic}
-                </p>
-                <div className="card-body">
+
+                <div className="flex flex-wrap px-5">
+                  {blog.topics.map((topic, index) => (
+                    <p
+                      key={index}
+                      className="text-blue font-bold text-14 px-2 py-3 badge badge-outline border-none rounded bg-[#E0E6F7]"
+                    >
+                      {topic}
+                    </p>
+                  ))}
+                </div>
+
+                <div className="card-body p-4 gap-1">
                   <h2 className="card-title text-lg font-bold">{blog.title}</h2>
                   <p className="text-sm font-medium">{blog.description}</p>
 
-                  <div className="flex justify-between mt-4">
-                    <div className="flex gap-2">
+                  <div className="flex justify-between items-center pt-2">
+                    <div className="flex items-center gap-4 pt-2">
                       <img
-                        className="w-[35px] h-[35px] rounded-lg mt-2"
+                        className="w-12 h-12 rounded-lg"
                         src="https://secure.gravatar.com/avatar/bb00cf69a3ae5d10043a9e2d3db9b173?s=64&d=mm&r=g"
                         alt=""
                       />
@@ -52,7 +60,6 @@ const BlogSection = () => {
                     </div>
                     <div className="card-actions justify-end">
                       <div className="">{blog.time_since_read}</div>
-                      {/* <div className="badge badge-outline">Products</div> */}
                     </div>
                   </div>
                 </div>

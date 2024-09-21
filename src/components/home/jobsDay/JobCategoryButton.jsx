a0384d8132d596a44c1ca81a6f9ebc6b966af179
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-21 13:26:02 +0600

    update: job category button design update

--- src/components/home/jobsDay/JobCategoryButton.jsx
+++ src/components/home/jobsDay/JobCategoryButton.jsx
@@ -8,15 +8,15 @@ function JobCategoryButton({
   return (
     <button
       onClick={() => handleCategoryClick(categoryItem)}
-      className={`bg-blue border border-lightGray rounded-lg  px-4 py-2 lg:px-6 lg:py-4 
+      className={`bg-blue order-[.5px] rounded-lg text-14  px-4 py-2 lg:px-6 lg:py-4 
         ${
           activeCategory === categoryItem
-            ? "text-white  bg-[#F8FAFF] border-blue"
-            : "bg-white text-blue border-[#B4C0E0]  px-4 py-2 lg:px-6 lg:py-4"
+            ? "text-white  bg-[#F8FAFF]"
+            : "bg-softLightBlue border-[.5px] border-lightGray hover:border-blue text-blue  px-4 py-2 lg:px-6 lg:py-4"
         }`}
     >
       <img
-        src={categorySVGs[categoryItem]} // Use the correct category SVG
+        src={categorySVGs[categoryItem]} 
         alt={`${categoryItem} icon`}
         className="inline-block w-6 h-6 mr-2"
       />

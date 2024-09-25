5037b9f6681c1e510eea4732d21b1731ae022bb8
Ayub40 <ayubk4028@gmail.com>
2024-09-26 02:22:22 +0600

    added responsive (blog page)

--- src/pages/blogs/BlogsPage.jsx
+++ src/pages/blogs/BlogsPage.jsx
@@ -33,7 +33,9 @@ function BlogsPage() {
   }, []);
 
   return (
+    // changed this line 
     <div className="">
+      {/* <div className=""> */}
       <Helmet>
         <title>Hiring Staff - About Us</title>
       </Helmet>
@@ -44,13 +46,15 @@ function BlogsPage() {
       />
       {/* bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] ,,,,,linear-gradient(to right, #151515, rgba(21, 21, 21, 0)*/}
 
-      <div className="container gap-24 grid lg:grid-cols-3 ">
+      {/* <div className="container grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 "> */}
+      {/* changed now */}
+      <div className="mx-auto container p-4 grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-10">
         {
           contents.slice(5, 8).map((content) => (
             <Link key={content.id} to={`/blog-details/${content.id}`}>
               <div
                 // key={id}
-                className="h-[611px] w-[450px] hover:-translate-y-1 transition duration-300 bg-cover bg-center rounded-2xl relative bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]"
+                className="h-[400px] md:h-[500px] lg:h-[611px] w-full hover:-translate-y-1 transition duration-300 bg-cover bg-center rounded-2xl relative bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]"
                 style={{
                   backgroundImage: `url(${content.image})`
                 }}
@@ -77,11 +81,14 @@ function BlogsPage() {
       </div>
 
       {/* Blog card */}
-      <div className="grid grid-cols-3 border container border-yellow-500">
-        <div className=" grid lg:grid-cols-2 col-span-2  gap-y-5">
+      {/* <div className="grid grid-cols-3 border container border-yellow-500"> */}
+      <div className="flex flex-col lg:flex-row gap-6 container">
+        {/* <div className="w-2/3 grid lg:grid-cols-2 col-span-2 gap-y-5"> */}
+        {/* now changed */}
+        <div className="w-full lg:w-2/3 grid grid-cols-1 sm:grid-cols-2 gap-6">
           {
             contents.map((content, id) => (
-              <Link to={`/blog-details/${content.id}`} key={id} className="border-[.5px] border-lightGray hover:border-blue rounded-lg px-1 py-3 h-[559px] w-[420px] hover:-translate-y-1 transition duration-300">
+              <Link to={`/blog-details/${content.id}`} key={id} className="border-[.5px] border-lightGray hover:border-blue rounded-lg px-1 py-3  hover:-translate-y-1 transition duration-300 h-[600px] md:h-[500px] lg:h-[611px] w-full">
                 <figure className="p-4">
                   <img className="rounded-lg w-full" src={content.image} alt="Blog" />
                 </figure>
@@ -104,17 +111,17 @@ function BlogsPage() {
                   <div className="flex justify-between items-end mt-6">
                     <div className="flex items-center gap-4">
                       <img
-                        className="w-12 h-12 rounded-full"
+                        className="lg:w-12 w-8 lg:h-12 h-8  rounded-full"
                         src="https://secure.gravatar.com/avatar/bb00cf69a3ae5d10043a9e2d3db9b173?s=64&d=mm&r=g"
                         alt=""
                       />
                       <div>
-                        <h5 className="text-14">{content.writer_name}</h5>
-                        <p className="text-12 mt-1">{content.date}</p>
+                        <h5 className="lg:text-14 text-xs">{content.writer_name}</h5>
+                        <p className="lg:text-12 text-[11px] lg:mt-1">{content.date}</p>
                       </div>
                     </div>
                     <div className="text-12 mb-2">
-                      <div className="">{content.time_since_read}</div>
+                      <div className="text-xs">{content.time_since_read}</div>
                     </div>
                   </div>
                 </div>
@@ -124,9 +131,13 @@ function BlogsPage() {
           }
 
         </div>
-        <div className="col-span-1 ">
+        {/* <div className="col-span-1 "> */}
+        {/* <div className="w-1/3 "> */}
+        {/* Now changed */}
+        <div className="w-full lg:w-1/3 space-y-10">
           <div className="h-28 w-auto border border-lightGray rounded-lg relative">
             <input type="text" placeholder="" className="h-16 w-[345px] mt-6 ml-10 border border-lightGray rounded-lg " />
+            {/* <input type="text" placeholder="" className="w-full p-3 rounded-lg border border-gray-300 focus:outline-none" /> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -138,7 +149,8 @@ function BlogsPage() {
                 clipRule="evenodd" />
             </svg>
           </div>
-          <div className=" h-[700px] w-auto border border-lightGray hover:border-blue  rounded-lg mt-10 shadow-sm">
+          <div className="border-lightGray hover:border-blue mt-10 border rounded-lg p-6 shadow-sm">
+            {/* <div className=" h-[700px] w-auto border border-lightGray hover:border-blue  rounded-lg mt-10 shadow-sm"> */}
             {/* start */}
             <div className=" items-end mt-1">
               <h4 className="mr-3 mb-5 mt-3 ml-4 pb-2 border-b-2 border-lightGray ">Recent Posts</h4>
@@ -169,8 +181,9 @@ function BlogsPage() {
             {/* start end */}
           </div>
           {/* HIRING Part */}
-          <div className="h-[600px] border border-lightGray hover:border-blue rounded-lg mt-10 shadow-sm ">
-            <div className="h-[550px] w-[400px] m-auto border rounded-lg mt-5 bg-[#F2F6FD] shadow-sm border-lightGray pl-10 space-y-4 relative">
+          {/* <div className="border rounded-lg p-6 bg-[#F2F6FD] relative h-[600px] mt-10"> */}
+          <div className="lg:h-[600px]  border border-lightGray hover:border-blue rounded-lg mt-10 shadow-sm ">
+            <div className="lg:h-[550px] h-[400px] lg:w-[400px] lg:m-auto m-5 border rounded-lg bg-[#F2F6FD] shadow-sm border-lightGray lg:pl-10 p-3  space-y-4 relative">
               <h3 className="text-[#B4C0E0] text-2xl font-light  pt-9">WE ARE <br /><span className="text-4xl font-extrabold text-[#66789C]">HIRING</span></h3>
               <p className="text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae architecto</p>
               <button className=" border-solid  text-xs px-2 py-2 rounded-md text-white bg-[#66789C] hover:text-[#66789C] hover:bg-white">Know More</button>
@@ -181,9 +194,9 @@ function BlogsPage() {
 
           </div>
           {/* Gallery part */}
-          <div className=" h-[530px] border rounded-lg mt-10 shadow-sm border-lightGray hover:border-blue">
+          <div className=" lg:h-[530px] border rounded-lg mt-10 shadow-sm border-lightGray hover:border-blue">
             <h4 className="mr-3 mb-5 mt-3 ml-4 pb-2 border-b-2 border-lightGray ">Gallery</h4>
-            <div className="grid grid-cols-3 gap-3 pl-3 mt-2 pr-3">
+            <div className="grid grid-cols-3 gap-3 pl-3 mt-2 pr-3 lg:pb-3 pb-3">
               <img className="object-cover w-full" src={galleryPic1} alt="" />
               <img className="object-cover w-full" src={galleryPic2} alt="" />
               <img className="object-cover w-full" src={galleryPic3} alt="" />

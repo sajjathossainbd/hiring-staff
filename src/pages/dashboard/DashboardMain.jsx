997f02ddf91370facb3f40ce0c0e65d35f7813a3
dev-kawser <kawserofficial.dev@gmail.com>
2024-09-26 00:37:12 +0600

    Feature: Chart implement successfuly in dashboard

--- src/pages/dashboard/DashboardMain.jsx
+++ src/pages/dashboard/DashboardMain.jsx
@@ -1,11 +1,59 @@
 import useAuth from "../../hooks/useAuth";
 import DashboardCard from "./shared/DashboardCard";
 import TinnyHeading from "./shared/TinnyHeading";
-
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const DashboardMain = () => {
 
     const { user } = useAuth()
+
+
+
+    const data = [
+        {
+            name: 'Page A',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'Page B',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'Page C',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: 'Page D',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'Page E',
+            uv: 1890,
+            pv: 4800,
+            amt: 2181,
+        },
+        {
+            name: 'Page F',
+            uv: 2390,
+            pv: 3800,
+            amt: 2500,
+        },
+        {
+            name: 'Page G',
+            uv: 3490,
+            pv: 4300,
+            amt: 2100,
+        },
+    ];
+
 
     return (
         <div>
@@ -20,7 +68,30 @@ const DashboardMain = () => {
                 <DashboardCard title={'Message'} quantity={'1000'} />
                 <DashboardCard title={'Shortlisted'} quantity={'50'} />
             </div>
-        </div>
+            <div style={{ height: '400px' }} className="mt-10">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+                        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+        </div >
     );
 };
 

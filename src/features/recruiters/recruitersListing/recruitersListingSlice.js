e72429083f520230e71fe67ce4c9b4f71a7eee84
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-30 02:02:44 +0600

    redux: create reacruiter slice and data fetch ui show

---
+++ src/features/recruiters/recruitersListing/recruitersListingSlice.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getRecruitersListing } from "./recruitersListingAPI";
+
+const initialState = {
+  recruitersListing: [],
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+// async thunk
+export const fetchRecruitersListing = createAsyncThunk(
+  "recruitersListing/fetchRecruitersListing",
+  async () => {
+    const recruitersListing = await getRecruitersListing();
+    return recruitersListing;
+  }
+);
+
+const recruitersListingSlice = createSlice({
+  name: "recruitersListing",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRecruitersListing.pending, (state) => {
+        state.isError = false;
+        state.isLoading = true;
+      })
+      .addCase(fetchRecruitersListing.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.recruitersListing = action.payload;
+      })
+      .addCase(fetchRecruitersListing.rejected, (state, action) => {
+        state.isLoading = false;
+        state.recruitersListing = [];
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default recruitersListingSlice.reducer;

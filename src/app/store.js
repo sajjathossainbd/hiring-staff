d6b5b14c6a69332d2a92853f43c55327b7ac1c9a
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-29 10:06:38 +0600

    redux: create similar jobs slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobsListingReducer from "../features/jobs/jobsListing/jobsListingSlice";
 import jobDetailsReducer from "../features/jobs/jobsDetails/jobDetailsSlice";
+import similarReducer from "../features/jobs/similarJobs/similarJobsSlice";
 
 const store = configureStore({
   reducer: {
     jobsListing: jobsListingReducer,
     jobDetails: jobDetailsReducer,
+    similarJobs: similarReducer,
   },
 });
 

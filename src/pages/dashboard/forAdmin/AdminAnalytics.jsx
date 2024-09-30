0f6662df6c8658b709709023d010cb8fb6e455b3
dev-kawser <kawserofficial.dev@gmail.com>
2024-09-30 10:40:01 +0600

    update: create a component and update the main dashboard chart

--- src/pages/dashboard/forAdmin/AdminAnalytics.jsx
+++ src/pages/dashboard/forAdmin/AdminAnalytics.jsx
@@ -4,69 +4,10 @@ import TinnyHeading from "../shared/TinnyHeading";
 import { IoBagRemoveOutline } from "react-icons/io5";
 import { VscGitStashApply } from "react-icons/vsc";
 import { MdPlaylistAddCheck } from "react-icons/md";
-
-import {
-    LineChart,
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    AreaChart,
-    Area,
-    ResponsiveContainer,
-} from 'recharts';
+import StackedBarChart from "../../../components/dashboard/StackedBarChart";
 
 
 const AdminAnalytics = () => {
-
-    const data = [
-        {
-            name: 'Users',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Apply',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Jobs',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Recruiters',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Candidates',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Shortlists',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Message',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
-
-
 
     return (
         <div>
@@ -82,58 +23,7 @@ const AdminAnalytics = () => {
                 <DashboardCard logo={<VscGitStashApply />} title={'All Applications'} quantity={'1000'} />
                 <DashboardCard logo={<MdPlaylistAddCheck />} title={'Shortlisted'} quantity={'50'} />
             </div>
-            <div style={{ width: '100%' }} className="mt-10">
-                <h4 className="mt-5">A areaChart of admin analytics</h4>
-
-                <div className="mt-7">
-                    <ResponsiveContainer width="100%" height={200}>
-                        <LineChart
-                            width={500}
-                            height={200}
-                            data={data}
-                            syncId="anyId"
-                            margin={{
-                                top: 10,
-                                right: 30,
-                                left: 0,
-                                bottom: 0,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Line type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-                        </LineChart>
-                        <Legend />
-                    </ResponsiveContainer>
-                </div>
-
-                <div className="mt-7">
-                    <ResponsiveContainer width="100%" height={200}>
-                        <AreaChart
-                            width={500}
-                            height={200}
-                            data={data}
-                            syncId="anyId"
-                            margin={{
-                                top: 10,
-                                right: 30,
-                                left: 0,
-                                bottom: 0,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-                        </AreaChart>
-                    </ResponsiveContainer>
-                </div>
-            </div>
+            <StackedBarChart />
         </div>
     );
 };

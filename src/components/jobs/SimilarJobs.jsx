eedca9943f0167aab0442d6f3d4d93f2b8cac0ed
Israfil6580 <israfilhossainmilon@gmail.com>
2024-10-09 15:38:08 +0600

    Redesign: job details design

--- src/components/jobs/SimilarJobs.jsx
+++ src/components/jobs/SimilarJobs.jsx
@@ -1,18 +1,44 @@
+import { CiLocationOn } from "react-icons/ci";
+import { GoDotFill } from "react-icons/go";
+
 function SimilarJobs() {
+  const arr = {
+    brandImage:
+      "https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg",
+    position: "Frontend Developer",
+    type: "Fulltime",
+    posted: "10/10/23",
+  };
   return (
-    <div className="border-t-[1px] border-t-lightGray pt-4 pb-2">
-      <div className="flex gap-5 items-center">
-        <img className="h-14 w-14 object-cover" src={arr.brandImage} alt="" />
-        <div>
-          <h5 className="pb-1">{arr.position}</h5>
-          <div className="flex justify-between">
-            <p className="text-sm">{arr.type}</p>
-            <p className="text-sm">{arr.posted}</p>
+    <div className="border rounded-xl border-bgDeepBlue p-4">
+      <div className="flex justify-end font-medium text-[13px]">2 days ago</div>
+      <div>
+        <div className="flex gap-5 items-start">
+          <img
+            className="h-14 w-auto object-cover"
+            src={arr.brandImage}
+            alt=""
+          />
+          <div>
+            <h5 className="pb-1">{arr.position}</h5>
+            <div className="flex items-center gap-x-2 text-14">
+              <span className="text-blue font-medium">Company Name</span>
+              <GoDotFill className="text-[8px] text-gray" />
+              <div className="flex items-center gap-x-1">
+                <CiLocationOn />
+                <span>Dhaka , Bangladesh</span>
+              </div>
+            </div>
+            <div className="flex gap-2 mt-3">
+              <span className="px-2 py-1 bg-bgLightWhite text-14 font-medium rounded-md">
+                Fulltime
+              </span>
+              <span className="px-2 py-1 bg-bgLightWhite text-14 font-medium rounded-md">
+                Remote
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <p className="text-sm text-end pt-2">{arr.location}</p>
       </div>
     </div>
   );
--- src/pages/jobs/JobDetails.jsx
+++ src/pages/jobs/JobDetails.jsx
@@ -6,6 +6,10 @@ import { fetchJobDetails } from "../../features/jobs/jobsDetails/jobDetailsSlice
 import Loading from "../../components/ui/Loading";
 import NoFoundData from "../../components/ui/NoFoundData";
 import { CiBadgeDollar } from "react-icons/ci";
+import { GoDotFill } from "react-icons/go";
+import { CiLocationOn } from "react-icons/ci";
+import { FaBookmark } from "react-icons/fa";
+import SimilarJobs from "../../components/jobs/SimilarJobs";
 function JobDetails() {
   const dispatch = useDispatch();
   const { id } = useParams();
@@ -16,8 +20,15 @@ function JobDetails() {
     isError,
     error,
   } = useSelector((state) => state.jobDetails);
+  // const {
+  //   recruitersListing: recruiters,
+  //   isLoading: isLoadingRecruiters,
+  //   isError: isErrorRecruiters,
+  //   error: errorRecruiters,
+  // } = useSelector((state) => state.recruitersListing);
+
   const {
-    description,
+    description = [],
     job_type,
     job_location,
     postedDate,
@@ -35,6 +46,9 @@ function JobDetails() {
   useEffect(() => {
     dispatch(fetchJobDetails(id));
   }, [dispatch, id]);
+  // useEffect(() => {
+  //   dispatch(fetchRecruitersListing());
+  // }, [dispatch]);
 
   let content = null;
   if (isLoading) content = <Loading />;
@@ -49,88 +63,91 @@ function JobDetails() {
   if (!isLoading && !isError && job?._id) {
     content = (
       <>
-        <div className="container">
-          {content}
-
-          {/* update dynamic date */}
-          <div>
-            {/* <img
-              className="lg:h-[400px] rounded-3xl object-cover w-full mb-10 overflow-hidden"
-              src={image}
-              alt={position}
-            /> */}
-            <div className="flex lg:flex-row flex-col gap-10 dark:text-white">
-              <div className="lg:w-8/12 w-full">
-                <h3 className="pt-4 pb-2">{jobTitle}</h3>
-                <div className="flex gap-16 text-sm">
-                  <p>Type: {job_type}</p>
-                  <p>Posted: {postedDate}</p>
-                </div>
-                <hr className="border-0 h-px bg-lightGray mt-3 mb-12" />
-                {/* <h4 className="pt-4 pb-2">{`Welcome to the ${company}`}</h4> */}
-                {description.map((des, index) => {
-                  return <span key={index}>{des}</span>;
-                })}
-                <h4 className="pt-4 pb-2">Ideal Candidate Profile</h4>
-                <ul>
-                  {requirements.map((req, index) => {
-                    return <li key={index}>{req}</li>;
-                  })}
-                </ul>
-                <h4 className="pt-4 pb-2">Key Responsibilities</h4>
-                <ul>
-                  {responsibilities.map((respon, index) => {
-                    return <li key={index}>{respon}</li>;
-                  })}
-                </ul>
-                <h4 className="pt-4 pb-2">Salary</h4>
-                <p className="flex gap-x-1 items-center">
-                  <CiBadgeDollar /> ${min_salary} - {max_salary}
-                </p>
-                <h4 className="pt-4 pb-2">Last Date To Apply</h4>
-                <p>{lastDateToApply}</p>
-                <h4 className="pt-4 pb-2">Job Location</h4>
-                <p>{job_location}</p>
-                <h4 className="pt-4 pb-2">Key Education</h4>
-                <p>{education}</p>
-                <h4 className="pt-4 pb-2">Contact Us</h4>
-                <p>
-                  Contact Us For any Query{" "}
-                  <span className="text-blue hover:border-b">
-                    {company_email}
-                  </span>
-                </p>
-                <p className="mt-10">
-                  Join us in shaping the future of digital design and enhancing
-                  user experiences at Jthemes!
-                </p>
-                <div className="mt-4 md:flex gap-x-2">
-                  Tags:{" "}
-                  {tags.map((tag, index) => (
-                    <div
-                      key={index}
-                      className="badge p-4 badge-neutral badge-outline md:m-0 m-1"
-                    >
-                      {tag}
+        <div className="flex gap-16">
+          <div className="md:w-2/3">
+            <div className="flex justify-between">
+              <div>
+                <h4 className="mb-5">UI/UX Designer</h4>
+                <div className="flex items-center gap-2">
+                  <img
+                    src="https://jobpilot.templatecookie.com/dummy-data/images/companies/company-logo-01.jpg"
+                    alt=""
+                  />
+                  <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-x-2">
+                      <span className="text-blue font-medium">
+                        Company Name
+                      </span>
+                      <GoDotFill className="text-[8px] text-gray" />
+                      <div className="flex items-center gap-x-1">
+                        <CiLocationOn />
+                        <span>Dhaka , Bangladesh</span>
+                      </div>
                     </div>
-                  ))}
+                    <div className="flex gap-2">
+                      <span className="px-2 py-1 bg-bgLightWhite text-14 font-medium rounded-md">
+                        Fulltime
+                      </span>
+                      <span className="px-2 py-1 bg-bgLightWhite text-14 font-medium rounded-md">
+                        Remote
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
-
-              <div className="lg:w-4/12 flex flex-col gap-10 w-full">
-                {/* Similar Jobs */}
-                <div className="border border-lightGray p-6 rounded-xl">
-                  <h4 className="pb-7">Similar Jobs</h4>
-                  {/*  */}
-                </div>
+              <div className="flex gap-x-2">
+                <button className="btn btn-primary bg-blue px-6 min-h-[2.8rem] h-[2.8rem] rounded-xl">
+                  Apply Now
+                </button>
+                <button className="btn btn-primary btn-outline min-h-[2.8rem] h-[2.8rem] px-3 border-bgDeepBlue text-blue rounded-xl text-lg">
+                  <FaBookmark />
+                </button>
               </div>
             </div>
+            <div className="mt-7">
+              <h5 className="mb-2">About this role</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+                cumque amet ipsa sapiente corporis dolore illum soluta! Ipsa
+                tempore quibusdam maiores nesciunt, omnis dolore consectetur vel
+                ipsum deleniti, nisi sequi. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Tenetur cumque amet ipsa sapiente
+                corporis dolore illum soluta! Ipsa tempore quibusdam maiores
+                nesciunt, omnis dolore consectetur vel ipsum deleniti, nisi
+                sequi.
+              </p>
+            </div>
+            <div className="mt-7">
+              <h5 className="mb-2">Requirement</h5>
+              <ul>
+                <li className="flex items-center gap-x-1">
+                  <GoDotFill className="text-[10px] text-gray" />
+                  hello this israfil hossain milon
+                </li>
+              </ul>
+            </div>
+            <div className="mt-7">
+              <h5 className="mb-2">Responsibility</h5>
+              <ul>
+                <li className="flex items-center gap-x-1">
+                  <GoDotFill className="text-[10px] text-gray" />
+                  hello this israfil hossain milon
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="md:w-1/3 flex flex-col gap-3">
+            <h4 className="mb-4">Similar Jobs</h4>
+            <SimilarJobs />
+            <SimilarJobs />
+            <SimilarJobs />
+            <SimilarJobs />
           </div>
         </div>
       </>
     );
   }
-  return <>{content}</>;
+  return <div className="container">{content}</div>;
 }
 
 export default JobDetails;

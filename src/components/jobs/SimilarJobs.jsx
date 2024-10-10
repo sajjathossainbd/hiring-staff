f0e231a48d1c50e3ee7288fe8ae57bfb4ce5ec30
Israfil6580 <israfilhossainmilon@gmail.com>
2024-10-10 08:09:06 +0600

    solve: jobdetails page

--- src/components/jobs/SimilarJobs.jsx
+++ src/components/jobs/SimilarJobs.jsx
@@ -1,40 +1,43 @@
+/* eslint-disable react/prop-types */
 import { CiLocationOn } from "react-icons/ci";
 import { GoDotFill } from "react-icons/go";
 
-function SimilarJobs() {
-  const arr = {
-    brandImage:
-      "https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg",
-    position: "Frontend Developer",
-    type: "Fulltime",
-    posted: "10/10/23",
-  };
+function SimilarJobs({
+  jobtitle,
+  brandImage,
+  type,
+  howManyDatesAgo,
+  company,
+  jobLocation,
+  country,
+  city,
+}) {
   return (
     <button className="btn h-auto block text-start bg-white dark:bg-darkBlue border rounded-xl border-bgDeepBlue p-4 hover:bg-bgLightWhite hover:border-bgLightWhite">
-      <div className="flex justify-end font-medium text-[13px]">2 days ago</div>
+      <div className="flex justify-end font-medium text-[13px]">
+        {howManyDatesAgo} days ago
+      </div>
       <div>
         <div className="flex gap-5 items-start">
-          <img
-            className="h-14 w-auto object-cover"
-            src={arr.brandImage}
-            alt=""
-          />
+          <img className="h-14 w-auto object-cover" src={brandImage} alt="" />
           <div>
-            <h5 className="pb-1">{arr.position}</h5>
+            <h5 className="pb-1">{jobtitle}</h5>
             <div className="flex items-center gap-x-2 text-14 mt-1">
-              <span className="text-blue font-medium">Company Name</span>
+              <span className="text-blue font-medium">{company}</span>
               <GoDotFill className="text-[8px] text-gray" />
               <div className="flex items-center gap-x-1 dark:text-white">
                 <CiLocationOn />
-                <span>Dhaka , Bangladesh</span>
+                <span>
+                  {city} , {country}
+                </span>
               </div>
             </div>
             <div className="flex gap-2 mt-3">
               <span className="px-2 py-1 bg-bgLightWhite text-14 font-medium rounded-md">
-                Fulltime
+                {type}
               </span>
               <span className="px-2 py-1 bg-bgLightWhite text-14 font-medium rounded-md">
-                Remote
+                {jobLocation}
               </span>
             </div>
           </div>

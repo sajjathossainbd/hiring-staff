5608c4d6a40346054d2e05e66b940ae6fb525a16
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-10-15 14:53:18 +0600

    update: job card design

--- src/components/shared/JobCard.jsx
+++ src/components/shared/JobCard.jsx
@@ -72,7 +72,7 @@ function JobCard({ job }) {
               <SecondaryButton title={"Apply Now"} />
             </Link>
           </div>
-          <div className="hover:bg-bgDeepBlue p-3 rounded-md hover:text-blue text-gray cursor-pointer hover:font-bold">
+          <div className="hover:bg-bgDeepBlue p-2 rounded-md hover:text-blue text-gray cursor-pointer hover:font-bold">
             <CiBookmark className="text-2xl" />
           </div>
         </div>

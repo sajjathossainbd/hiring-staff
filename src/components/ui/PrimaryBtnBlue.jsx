2553e158c6cac856b368f4e9a2d4848961e6eb0e
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-10-24 16:55:49 +0600

    github issues

--- src/components/ui/PrimaryBtnBlue.jsx
+++ src/components/ui/PrimaryBtnBlue.jsx
@@ -2,9 +2,10 @@
 function PrimaryBtnBlue({ icon, title }) {
   return (
     <button
-      className={`flex items-center justify-center gap-3 bg-blue hover:bg-darkBlue dark:hover:bg-white text-white px-7 py-4 rounded-md font-medium transition-all duration-500 ease-in-out `}
+      className={`flex items-center justify-center gap-3 bg-blue hover:bg-darkBlue dark:hover:bg-white text-white
+      px-3 py-3  md:px-5 md:py-4 lg:px-7 lg:py-4 rounded-md font-medium transition-all duration-500 ease-in-out `}
     >
-      <p className="text-16 text-white">{title}</p>
+      <p className="text-12 text-white">{title}</p>
       <div className="text-xl">{icon}</div>
     </button>
   );

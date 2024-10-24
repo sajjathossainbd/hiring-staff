1800e74b793bc3281123aca8784c836542a9604f
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-10-24 11:39:58 +0600

    update: sub navbar mobile number update

--- src/pages/shared/SubNavbarRight.jsx
+++ src/pages/shared/SubNavbarRight.jsx
@@ -2,42 +2,35 @@ import { FiPhoneCall } from "react-icons/fi";
 import DropdownSimple from "../../components/ui/DropdownSimple";
 import { Trans, useTranslation } from "react-i18next";
 
-const countryOptions = ["Bangladesh", "US", "Other"];
-const currencyOptions = ["EUR", "BD", "AURO"];
 const languageOptions = ["English", "Bangla"];
-
-
 
 function SubNavbarRight() {
   const { i18n } = useTranslation();
 
   const handleLanguageChange = (selectedLanguage) => {
-    console.log('Language selected:', selectedLanguage);
+    console.log("Language selected:", selectedLanguage);
     if (selectedLanguage === "English") {
       i18n.changeLanguage("en");
-      localStorage.setItem('language', 'en');
+      localStorage.setItem("language", "en");
     } else if (selectedLanguage === "Bangla") {
       i18n.changeLanguage("bn");
-      localStorage.setItem('language', 'bn');
+      localStorage.setItem("language", "bn");
     }
   };
-  
-  
+
   return (
     <div className="xl:flex lg:hidden md:hidden items-center gap-6 hidden">
       <div className="flex gap-2 items-center text-darkBlue">
         <FiPhoneCall />
-        <p className="text-darkBlue"><Trans i18nKey={"number"}></Trans></p>
+        <p className="text-darkBlue">
+          <Trans i18nKey={"number"}></Trans>
+        </p>
       </div>
-      <DropdownSimple dynamicOptions={currencyOptions} placeholderData="USD" />
+
       <DropdownSimple
-        dynamicOptions={countryOptions}
-        placeholderData="All Country"
-      />
-      <DropdownSimple
-        dynamicOptions={languageOptions} 
-        placeholderData={i18n.language === "en" ? "English" : "বাংলা"}  
-        onOptionSelect={handleLanguageChange} 
+        dynamicOptions={languageOptions}
+        placeholderData={i18n.language === "en" ? "English" : "বাংলা"}
+        onOptionSelect={handleLanguageChange}
       />
     </div>
   );

f287c0efed7d8a39ca311d40e4e03543be496bb9
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-10-28 03:00:48 +0600

    job appliers card desing

--- src/pages/dashboard/forRecruiter/JobAppliers.jsx
+++ src/pages/dashboard/forRecruiter/JobAppliers.jsx
@@ -1,5 +1,51 @@
 function JobAppliers() {
-  return <div>JobAppliers</div>;
+  return (
+    <div className="p-6 bg-gray-50">
+      <h1 className="text-2xl font-semibold text-gray-700 mb-4">
+        All Apply Candidates List
+      </h1>
+      {/* All Candidates List */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Details</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">2 year experience</div>
+                  </div>
+                </div>
+              </td>
+              <td>25 October, 2024</td>
+              <td>View Details</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default JobAppliers;

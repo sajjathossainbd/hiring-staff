8621b04996b8e9fd7d3bb3da8aa9696915bbc3c2
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-10-29 02:49:02 +0600

    update: shortlisted jobs card desing change

--- src/pages/dashboard/forCandidates/ShortlistedJobs.jsx
+++ src/pages/dashboard/forCandidates/ShortlistedJobs.jsx
@@ -8,9 +8,13 @@ import { CardPagination } from "../../../components/shared/CardPagination";
 import { Link, useNavigate } from "react-router-dom";
 import { FaRegEye } from "react-icons/fa";
 import useCurrentUser from "../../../hooks/useCurrentUser";
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { MdOutlineMailOutline } from "react-icons/md";
+import PrimaryBtnBlue from "../../../components/ui/PrimaryBtnBlue";
+import { VscEye } from "react-icons/vsc";
+import { AiOutlineUserDelete } from "react-icons/ai";
 
 const ShortlistedJobs = () => {
-
   const { currentCandidate } = useCurrentUser();
 
   const navigate = useNavigate();
@@ -56,48 +60,70 @@ const ShortlistedJobs = () => {
       />
 
       <div className="grid gap-3 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-6 mb-5">
-        {shortlistAppliedJobs?.jobs?.map((job, idx) => (
-          <div
-            key={idx}
-            className="shadow-md bg-white hover:-translate-y-1 duration-200 rounded-lg p-6 overflow-auto "
-          >
-            <div>
+        {shortlistAppliedJobs?.jobs?.map((job, idx) => {
+          const appliedDate = new Date(job.appliedDate).toLocaleDateString(
+            "en-GB",
+            {
+              timeZone: "Asia/Dhaka",
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            }
+          );
+          return (
+            <div
+              key={idx}
+              className="shadow-md bg-white hover:-translate-y-1 duration-200 rounded-lg p-6 overflow-auto "
+            >
+              {/* Icon and status button */}
+              <div className="flex gap-8 items-center mb-4">
+                <div className="bg-bgLightWhite p-3 text-blue rounded-md text-2xl inline-block">
+                  <IoBriefcaseOutline />
+                </div>
+                <button className="bg-bgLightWhite text-blue font-medium rounded-full text-14 px-6 py-1 pb-2">
+                  {job?.shortlist}
+                </button>
+              </div>
+
+              {/* Job title, company name, and view details */}
               <div className="flex items-center justify-between">
-                <div className="ml-3">
-                  <h5 className="">{job.jobTitle}</h5>
-                  <div className="flex flex-wrap text-12 text-gray">
-                    <span className="flex items-center mr-4 flex-wrap ">
-                      <FaWarehouse className="mr-1 text-lightBlue" />{" "}
+                <div className="">
+                  <h5 className="text-lg font-semibold mb-2">{job.jobTitle}</h5>
+                  <div className="flex flex-wrap text-16 text-gray">
+                    <span className="flex items-center gap-2">
+                      <MdOutlineMailOutline />
                       {job.company_email}
                     </span>
                   </div>
                 </div>
+              </div>
+
+              {/* Applied date*/}
+              <div className="mt-1 flex items-center justify-between">
+                <div>
+                  <p className="text-14">
+                    Applied Date:
+                    <span className="bg-green-100 text-green-500  rounded-full ml-2">
+                      {appliedDate}
+                    </span>
+                  </p>
+                </div>
+              </div>
+
+              {/* View Details Job & Delete Action */}
+              <div className="flex items-center gap-6 mt-6">
                 <Link to={`/job-details/${job?.jobId}`}>
-                  <button className=" rounded-full text-blue hover:text-white hover:bg-blue">
-                    <FaRegEye />
+                  <button className="">
+                    <PrimaryBtnBlue
+                      icon={<VscEye />}
+                      title={"See Details Job Post"}
+                    />
                   </button>
                 </Link>
               </div>
             </div>
-
-            <div className="mt-5 flex items-center justify-between">
-              <div>
-                <p className="text-12">
-                  Applied on:
-                  <span className="bg-green-100 text-green-500 px-3 py-1 rounded-full">
-                    {job?.appliedDate}
-                  </span>
-                </p>
-              </div>
-
-              <div className="flex space-x-4 text-gray">
-                <p className="text-12 bg-yellow-300 px-2 py-1 rounded-lg">
-                  {job?.shortlist}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       {/* Pagination Component */}

c9db5c6c3163e3ea579f36831b9f9b9f38666a62
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-10-31 15:58:44 +0600

    update: udpate job listing page, and candidate page desing and data change

--- src/components/home/category/CategoryCard.jsx
+++ src/components/home/category/CategoryCard.jsx
@@ -5,7 +5,7 @@ const CategoryCard = ({ categoryName, emoji, onCategoryClick }) => {
       className="boxBorderHoverBlue py-5 px-4 rounded-lg shadow-sm text-center flex justify-center items-center gap-2 cursor-pointer bg-bgLightBlue dark:bg-darkBlue text-white"
       onClick={() => onCategoryClick(categoryName)}
     >
-      <div className="flex items-center lg:flex-row flex-col gap-2">
+      <div className="md:flex lg:flex items-center lg:flex-row flex-col gap-y2 md:gap-x-2 lg:gap-x-2">
         <h3>{emoji}</h3>
         <h3 className="text-12 md:text-14">{categoryName}</h3>
       </div>
